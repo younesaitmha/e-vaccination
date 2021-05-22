355dbc1a339accf7b8d75e1cd350d8f1e1708483
younes ait m'ha <younesaitmha@gmail.com>
2021-05-22 23:40:27 +0100

    update models

--- models.py
+++ models.py
@@ -2,3 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql import func
 
 db = SQLAlchemy()
+
+class User(db.Model):
+    __tablename__ = 'users'
+    
--- test.py
+++
@@ -1,3 +0,0 @@
-import random, string
-
-print(''.join(random.choice(string.ascii_letters) for _ in range(22)))
